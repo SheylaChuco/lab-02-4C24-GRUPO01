cb032519b7eb9b80f407f26d2e9d817526b699b5
Sheyla <sheylachuco123@gmail.com>
2025-08-26 20:52:24 -0500

    Colaborador 1: agrega StudentModel.java

---
+++ StudentModel.java
@@ -0,0 +1,1 @@
+public class StudentModel {    private String name;    private int age;    public StudentModel(String name, int age) {        this.name = name;        this.age = age;    }    public void showInfo() {        System.out.println("Nombre: " + name + ", Edad: " + age);    }} 
