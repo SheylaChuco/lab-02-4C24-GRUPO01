6fcf6b848516a2bcec2b3bfa8b5d97205b0296dd
Deivid Flores <deividfloresvgs@gmail.com>
2025-08-26 22:15:32 -0500

    Colaborador: agrega método showSummary en StudentReport.java

--- StudentReport.java
+++ StudentReport.java
@@ -2,4 +2,9 @@ public class StudentReport {
     public void generateReport() {
         System.out.println("Generando reporte de estudiantes...");
     }
+
+    public void showSummary() {
+        System.out.println("Mostrando resumen de calificaciones...");
+    }
 }
+
