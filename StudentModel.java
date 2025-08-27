965f38edce36ec2c6353302f407e868c52b4897e
Sheyla <sheylachuco123@gmail.com>
2025-08-26 22:36:33 -0500

    feat: agregada función showWelcome en StudentModel

--- StudentModel.java
+++ StudentModel.java
@@ -1,1 +1,14 @@
-public class StudentModel {    private String name;    private int age;    public StudentModel(String name, int age) {        this.name = name;        this.age = age;    }    public void showInfo() {        System.out.println("Nombre: " + name + ", Edad: " + age);    }} 
+public class StudentModel {    private String name;    private int age;
+
+    public StudentModel(String name, int age) {
+        this.name = name;        this.age = age;
+    }
+    public void showInfo() {
+        System.out.println("Nombre: " + name + ", Edad: " + age);
+    }
+
+    // Nueva función: Verifica si el estudiante es mayor de edad
+    public boolean esMayorDeEdad() {
+        return age >= 18;
+    }
+}
